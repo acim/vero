15f4b9c7171df314697f77aedd51833fbc1cfa15
Boban Acimovic <boban.acimovic@gmail.com>
2021-10-10 13:05:45 +0200

    add tags response

--- src/dockerhub.rs
+++ src/dockerhub.rs
@@ -1,6 +1,8 @@
 //!
 use reqwest::Result;
 
+const PER_PAGE: u64 = 100;
+
 pub struct Repositories {
     owner: String,
     repositories: <Vec<Repository> as IntoIterator>::IntoIter,
@@ -17,7 +19,7 @@ impl Repositories {
             repositories: vec![].into_iter(),
             client: reqwest::blocking::Client::new(),
             page: 0,
-            per_page: 100,
+            per_page: PER_PAGE,
             total: 0,
         })
     }
@@ -39,7 +41,11 @@ impl Repositories {
 
         println!("url: {}", url);
 
-        let response = self.client.get(&url).send()?.json::<Data>()?;
+        let response = self
+            .client
+            .get(&url)
+            .send()?
+            .json::<RepositoriesResponse>()?;
         self.repositories = response.results.into_iter();
         self.total = response.count;
         Ok(self.repositories.next())
@@ -58,9 +64,10 @@ impl Iterator for Repositories {
     }
 }
 
+// Owner repositories
 #[derive(Default, Debug, Clone, PartialEq, serde_derive::Serialize, serde_derive::Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub struct Data {
+pub struct RepositoriesResponse {
     pub count: u64,
     pub next: ::serde_json::Value,
     pub previous: ::serde_json::Value,
@@ -96,4 +103,61 @@ pub struct Repository {
     pub affiliation: ::serde_json::Value,
     #[serde(rename = "hub_user")]
     pub hub_user: String,
+}
+
+// Repository tags
+#[derive(Default, Debug, Clone, PartialEq, serde_derive::Serialize, serde_derive::Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct TagsResponse {
+    pub count: i64,
+    pub next: String,
+    pub previous: ::serde_json::Value,
+    pub results: Vec<Tag>,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, serde_derive::Serialize, serde_derive::Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Tag {
+    pub creator: i64,
+    pub id: i64,
+    #[serde(rename = "image_id")]
+    pub image_id: ::serde_json::Value,
+    pub images: Vec<Image>,
+    #[serde(rename = "last_updated")]
+    pub last_updated: String,
+    #[serde(rename = "last_updater")]
+    pub last_updater: i64,
+    #[serde(rename = "last_updater_username")]
+    pub last_updater_username: String,
+    pub name: String,
+    pub repository: i64,
+    #[serde(rename = "full_size")]
+    pub full_size: i64,
+    pub v2: bool,
+    #[serde(rename = "tag_status")]
+    pub tag_status: String,
+    #[serde(rename = "tag_last_pulled")]
+    pub tag_last_pulled: String,
+    #[serde(rename = "tag_last_pushed")]
+    pub tag_last_pushed: String,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, serde_derive::Serialize, serde_derive::Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Image {
+    pub architecture: String,
+    pub features: String,
+    pub variant: ::serde_json::Value,
+    pub digest: String,
+    pub os: String,
+    #[serde(rename = "os_features")]
+    pub os_features: String,
+    #[serde(rename = "os_version")]
+    pub os_version: ::serde_json::Value,
+    pub size: i64,
+    pub status: String,
+    #[serde(rename = "last_pulled")]
+    pub last_pulled: String,
+    #[serde(rename = "last_pushed")]
+    pub last_pushed: String,
 }
