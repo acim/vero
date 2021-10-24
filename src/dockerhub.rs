1213eec3256b1d661c849e598c0c52f87804653f
Boban Acimovic <boban.acimovic@gmail.com>
2021-10-24 18:55:00 +0200

    remove excessive serde field attributes

--- src/dockerhub.rs
+++ src/dockerhub.rs
@@ -76,7 +76,6 @@ impl Client {
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
 pub struct Response<T> {
     pub count: u64,
     pub next: ::serde_json::Value,
@@ -85,79 +84,54 @@ pub struct Response<T> {
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
 pub struct Repository {
     pub user: String,
     pub name: String,
     pub namespace: String,
-    #[serde(rename = "repository_type")]
     pub repository_type: ::serde_json::Value,
     pub status: u64,
     pub description: String,
-    #[serde(rename = "is_private")]
     pub is_private: bool,
-    #[serde(rename = "is_automated")]
     pub is_automated: bool,
-    #[serde(rename = "can_edit")]
     pub can_edit: bool,
-    #[serde(rename = "star_count")]
     pub star_count: u64,
-    #[serde(rename = "pull_count")]
     pub pull_count: u64,
-    #[serde(rename = "last_updated")]
     pub last_updated: String,
-    #[serde(rename = "is_migrated")]
     pub is_migrated: bool,
-    #[serde(rename = "collaborator_count")]
     pub collaborator_count: u64,
     pub affiliation: ::serde_json::Value,
-    #[serde(rename = "hub_user")]
     pub hub_user: String,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
 pub struct Tag {
     pub creator: u64,
     pub id: u64,
-    #[serde(rename = "image_id")]
     pub image_id: ::serde_json::Value,
     pub images: Vec<Image>,
-    #[serde(rename = "last_updated")]
     pub last_updated: Option<String>,
-    #[serde(rename = "last_updater")]
     pub last_updater: u64,
-    #[serde(rename = "last_updater_username")]
     pub last_updater_username: String,
     pub name: String,
     pub repository: u64,
-    #[serde(rename = "full_size")]
     pub full_size: u64,
     pub v2: bool,
-    #[serde(rename = "tag_status")]
     pub tag_status: String,
-    #[serde(rename = "tag_last_pulled")]
     pub tag_last_pulled: Option<String>,
-    #[serde(rename = "tag_last_pushed")]
     pub tag_last_pushed: Option<String>,
 }
 
 #[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
 pub struct Image {
     pub architecture: String,
     pub features: String,
     pub variant: Option<String>,
     pub digest: Option<String>,
     pub os: String,
-    #[serde(rename = "os_features")]
     pub os_features: String,
-    #[serde(rename = "os_version")]
     pub os_version: ::serde_json::Value,
     pub size: u64,
     pub status: String,
-    #[serde(rename = "last_pulled")]
     pub last_pulled: Option<String>,
-    #[serde(rename = "last_pushed")]
     pub last_pushed: Option<String>,
 }
